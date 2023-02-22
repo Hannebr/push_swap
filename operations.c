5c13efaa767c37cfd140f1e20a14e0ff4af155d2
hannebr <hbrouwer@LAPTOP-0I7EPC45>
2023-02-22 15:13:28 +0100

    swap function added

--- operations.c
+++ operations.c
@@ -6,18 +6,37 @@
 /*   By: hbrouwer <hbrouwer@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/21 13:52:15 by hbrouwer      #+#    #+#                 */
-/*   Updated: 2023/02/21 15:28:49 by hbrouwer      ########   odam.nl         */
+/*   Updated: 2023/02/22 11:18:21 by anonymous     ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void    swap(t_stack *stack_a, t_stack *stack_b, int opt)
+void    swap(t_stack *stack_a, t_stack *stack_b, int option)
 {
-    if (opt == 0 || opt == 1)
+    if (option == 1)
     {
+        swap_stack(stack_a);
+        printf("sa\n");
     }
-    if (opt == 0 || opt == 2)
+    else if (option == 2)
     {
+        swap_stack(stack_b);
+        printf("sb\n")
     }
+    else if (option == 0)
+    {
+        swap_stack(stack_a);
+        swap_stack(stack_b);
+        printf("ss\n")
+    }
+}
+
+void    swap_stack(t_stack *stack)
+{
+    int temp;
+    
+    temp = (*stack->tail)->prev->number;
+    (*stack->tail)->prev->number = (*stack->tail)->number;
+    (*stack->tail)->number = temp;
 }
